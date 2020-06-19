5492f0b28b9da1f4c1b3c139842c814d6d18d923
Clint <cpitzak@gmail.com>
2020-06-19 07:42:23 -0700

    Side menu styling

--- src/app/side-menu/side-menu.component.ts
+++ src/app/side-menu/side-menu.component.ts
@@ -8,9 +8,12 @@ import { Router } from '@angular/router';
 })
 export class SideMenuComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  constructor(public router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  isActive(url) {
   }
 
 }
