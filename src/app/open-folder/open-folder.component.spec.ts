1122f2dc4472dd27ffeca3f4bbb0c7056af266ae
Clint <cpitzak@gmail.com>
2020-06-26 10:50:18 -0700

    Fix open folder test

--- src/app/open-folder/open-folder.component.spec.ts
+++ src/app/open-folder/open-folder.component.spec.ts
@@ -1,16 +1,35 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {RouterTestingModule} from "@angular/router/testing";
 
 import { OpenFolderComponent } from './open-folder.component';
+import { FormsModule, ReactiveFormsModule, FormControl, NgControl } from '@angular/forms';
 
 describe('OpenFolderComponent', () => {
   let component: OpenFolderComponent;
   let fixture: ComponentFixture<OpenFolderComponent>;
 
   beforeEach(async(() => {
+    const NG_CONTROL_PROVIDER = {
+      provide: NgControl,
+      useClass: class extends NgControl {
+      control = new FormControl();
+      viewToModelUpdate() {}
+      }
+    };
     TestBed.configureTestingModule({
-      declarations: [ OpenFolderComponent ]
+      declarations: [ OpenFolderComponent ],
+      providers: [NgControl],
+      imports: [
+        FormsModule,
+        ReactiveFormsModule,
+        RouterTestingModule
+      ],
+    })
+    .overrideComponent(OpenFolderComponent, {
+      add: { providers: [NG_CONTROL_PROVIDER] },
     })
     .compileComponents();
+
   }));
 
   beforeEach(() => {
