503d46066d94d9690abfdea7ebd17fc46e95e6ba
Clint <cpitzak@gmail.com>
2020-07-08 17:32:24 -0700

    Comment out logging

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,13 +14,13 @@ export class AppComponent {
     private translate: TranslateService
   ) {
     translate.setDefaultLang('en');
-    console.log('AppConfig', AppConfig);
+    // console.log('AppConfig', AppConfig);
 
     if (electronService.isElectron) {
-      console.log(process.env);
+      // console.log(process.env);
       console.log('Mode electron');
-      console.log('Electron ipcRenderer', electronService.ipcRenderer);
-      console.log('NodeJS childProcess', electronService.childProcess);
+      // console.log('Electron ipcRenderer', electronService.ipcRenderer);
+      // console.log('NodeJS childProcess', electronService.childProcess);
     } else {
       console.log('Mode web');
     }
